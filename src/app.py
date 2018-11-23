938998166c8af800f1e229508a825615f85a02b9
yishuihan <yishuihan@wacai.com>
2018-11-23 15:14:43 +0800

    set stdout to log

--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request
+import logging
 from image_classify.classify_api import classify
 
 app = Flask(__name__)
+URL_PREFIX = '/cifar-service'
 
-@app.route("/")
+@app.route(URL_PREFIX + "/")
 def index():
     return "Hello World!"
 
-@app.route('/cifar-service/classify', methods=['POST'])
+
+@app.route(URL_PREFIX + '/classify', methods=['POST'])
 def cls():
     if request.files.get('file'):
         file = request.files.get('file')
@@ -18,3 +21,7 @@ def cls():
 
 if __name__ == '__main__':
     app.run(debug=True)
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
